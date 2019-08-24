3431a714b1179076ba797a9555f9bcc3b7633522
Mark Wierda <97041029@st.deltion.nl>
2019-08-24 17:43:04 +0200

    Replaced coinmarketcap package

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const bittrex = require('node-bittrex-api');
-const Coinmarketcap = require('node-coinmarketcap-api');
+const Coinmarketcap = require('coinmarketcap-api');
 const mysql = require('mysql');
-const coinmarketcap = new Coinmarketcap();
+const coinmarketcap = new Coinmarketcap('API_KEY');
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
@@ -24,64 +24,51 @@ bittrex.getbalances((bittrex, err) => {
        let count = 0;
 
        bittrex.result.forEach(coin => {
-           const coins = {
-               'ADA': {'name': 'Cardano'},
-               'BAT': {'name': 'Basic Attention Token'},
-               'BTC': {'name': 'Bitcoin'},
-               'DAI': {'name': 'Dai'},
-               'DGB': {'name': 'DigiByte'},
-               'ETH': {'name': 'Ethereum'},
-               'EOS': {'name': 'EOS'},
-               'LTC': {'name': 'Litecoin'},
-               'RDD': {'name': 'ReddCoin'},
-               'USDT': {'name': 'Tether'},
-               'XLM': {'name': 'Stellar'},
-               'XRP': {'name': 'Ripple'}
-           };
+           const coins = ['ADA', 'BAT', 'BTC', 'DAI', 'DGB', 'ETH', 'EOS', 'LTC', 'RDD', 'USDT', 'XLM', 'XRP'];
 
-           if (coins[coin.Currency]) {
-               let fullname = coins[coin.Currency].name;
+           if (coins.indexOf(coin.Currency) > -1) {
+               coinmarketcap.getQuotes({symbol: coin.Currency, convert: 'EUR'}).then(coinmarketData => {
+                   coinmarketData = coinmarketData.data[coin.Currency];
 
-               (async () => {
-                   await coinmarketcap.ticker(fullname.replace(/ /g, '-'), 'EUR').then(coinmarketcap => {
-                       coinmarketcap = coinmarketcap[0];
+                   if (coinmarketData.name === 'XRP')
+                       coinmarketData.name = 'Ripple';
 
-                       connection.query('SELECT bought_for, highest_profit, lowest_profit FROM cryptocurrencies WHERE name = ?', [fullname], (err, res) => {
-                          if (err)
-                              console.error(err);
+                   connection.query('SELECT bought_for, highest_profit, lowest_profit FROM cryptocurrencies WHERE name = ?', coinmarketData.name, (err, res) => {
+                       if (err)
+                           console.error(err.toString());
 
-                          if (res) {
-                              res = res[0];
+                       if (res) {
+                           res = res[0];
 
-                              let bought_for = res.bought_for;
-                              let price = coin.Balance * coinmarketcap.price_eur;
-                              let profit = price - bought_for;
-                              let highest_profit = res.highest_profit;
-                              let lowest_profit = res.lowest_profit;
+                           if (res !== undefined) {
+                               let bought_for = res.bought_for;
+                               let price = coin.Balance * coinmarketData.quote['EUR'].price;
+                               let profit = price - bought_for;
+                               let highest_profit = res.highest_profit;
+                               let lowest_profit = res.lowest_profit;
 
-                              if (profit > highest_profit)
-                                  highest_profit = profit;
+                               if (profit > highest_profit)
+                                   highest_profit = profit;
 
-                              if (profit < lowest_profit)
-                                  lowest_profit = profit;
+                               if (profit < lowest_profit)
+                                   lowest_profit = profit;
 
-                              let query = 'UPDATE cryptocurrencies SET amount = ?, price = ?, profit = ?, highest_profit = ?, lowest_profit = ?, ' +
-                                  'difference_1h = ?, difference_24h = ?, difference_7d = ?, updated_at = ? WHERE name = ? AND active = 1';
-                              let data = [coin.Balance, price, profit, highest_profit, lowest_profit, coinmarketcap.percent_change_1h, coinmarketcap.percent_change_24h, coinmarketcap.percent_change_7d, new Date(), fullname];
+                               let query = 'UPDATE cryptocurrencies SET amount = ?, price = ?, profit = ?, highest_profit = ?, lowest_profit = ?, ' +
+                                   'difference_1h = ?, difference_24h = ?, difference_7d = ?, updated_at = ? WHERE name = ? AND active = 1';
+                               let data = [coin.Balance, price, profit, highest_profit, lowest_profit, coinmarketData.quote['EUR'].percent_change_1h, coinmarketData.quote['EUR'].percent_change_24h, coinmarketData.quote['EUR'].percent_change_7d, new Date(), coinmarketData.name];
 
-                              connection.query(query, data, (err, res) => {
-                                  if (err)
-                                      console.error(err);
+                               connection.query(query, data, (err, res) => {
+                                   if (err)
+                                       console.error(err.toString());
 
-                                  count++;
-
-                                  if (count === Object.keys(coins).length)
-                                      connection.end();
-                              });
-                          }
-                       });
+                                   count++;
+                                   if (count === Object.keys(coins).length)
+                                       connection.end();
+                               });
+                           }
+                       }
                    });
-               })();
+               }).catch(console.error);
            }
        });
    }
